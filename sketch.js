ee305a36f4422a4bd73e78bf856d7ae70fe1f04d
nvsc28 <00001113630425SP@al.educacao.sp.gov.br>
2024-05-29 13:37:01 -0300

    adicionando js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);
+    background("white")
+  }
+  
+  function draw() {
+    stroke("red");
+    fill("blue");
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }  
